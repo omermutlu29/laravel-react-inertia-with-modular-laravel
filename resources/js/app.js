a7d9e220e4f1b3d94693a8f66c8cbbc86bab50e2
jetteldevelopment <dev@jettel.com.tr>
2021-12-09 20:23:39 +0300

    First commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { InertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import * as Sentry from '@sentry/browser';
-
+const context = require.context("../../Modules",true,/Pages\/(.+)\.js$/,"lazy");
 InertiaProgress.init({
   color: '#ED8936',
   showSpinner: true
@@ -12,15 +12,24 @@ InertiaProgress.init({
 Sentry.init({
   dsn: process.env.MIX_SENTRY_LARAVEL_DSN
 });
-
 const app = document.getElementById('app');
+
+const init = JSON.parse(app.dataset.page);
+const resolver = (name) =>{
+  let parts = name.split('::');
+  console.log(parts,"asd");
+  if (parts.length > 1){
+    return context('./'+parts[0]+'/Resources/assets/js/Pages/'+parts[1]+'.js').then(module=>module.default);
+  }else{
+    return import('./Pages/'+name).then(module=>module.default);
+  }
+
+}
 
 render(
   <InertiaApp
-    initialPage={JSON.parse(app.dataset.page)}
-    resolveComponent={name =>
-      import(`./Pages/${name}`).then(module => module.default)
-    }
+    initialPage={init}
+    resolveComponent={resolver}
   />,
   app
 );
